8a3653d8bc57fe12d62d9cf512e3409e3ba46ed9
sourabh.saldi <Admin123#>
2020-07-21 22:15:55 +0530

    delete not functional as of now

--- src/datastructures/BinarySearchTree.java
+++ src/datastructures/BinarySearchTree.java
@@ -11,7 +11,6 @@ public class BinarySearchTree {
 
         Node(int data) {
             this.data = data;
-
         }
     }
 
@@ -36,13 +35,88 @@ public class BinarySearchTree {
         }
         if (prevNode == null)
             prevNode = node;
-        else if (data<prevNode.data) {
+        else if (data < prevNode.data) {
             prevNode.left = node;
         } else {
             prevNode.right = node;
         }
 
         return prevNode;
+    }
+
+
+    public Node findNode(int data) {
+        Node currentNode = rootNode;
+        while (currentNode != null) {
+            if (currentNode.data == data) {
+                return currentNode;
+            } else if (data < currentNode.data) {
+                currentNode = currentNode.left;
+            } else {
+                currentNode = currentNode.right;
+            }
+        }
+        return null;
+    }
+
+
+    public void delete(int data) {
+        Node currentNode = rootNode;
+        Node prevNode = rootNode;
+        boolean isLeft = false;
+
+        while (currentNode != null) {
+
+            if (currentNode.data == data) {
+                if (currentNode.left == null && currentNode.right == null) {
+                    //it is leaf node that is to be deleted
+                    if (isLeft) {
+                        prevNode.left = null;
+                    } else {
+                        prevNode.right = null;
+                    }
+
+                } else if (currentNode.left == null && currentNode.right != null) {
+                    //nothing on left but a tree on right
+                    prevNode.right = currentNode.right;
+
+                } else if (currentNode.right == null && currentNode.left != null) {
+                    //nothing on right but tree on left
+                    prevNode.left = currentNode.left;
+                } else {
+                    // tree on both sides
+                    Node node = leftMostOnRight(currentNode.right);
+
+                    currentNode.data = node.data;
+                    currentNode.left = prevNode.left;
+                    currentNode.right = prevNode.right;
+                }
+            } else if (data < currentNode.data) {
+                prevNode = currentNode;
+                isLeft = true;
+                currentNode = currentNode.left;
+            } else {
+                prevNode = currentNode;
+                isLeft = false;
+                currentNode = currentNode.right;
+            }
+        }
+    }
+
+    private Node leftMostOnRight(Node node) {
+        Node currentNode = node;
+
+        while (currentNode != null) {
+
+            if (currentNode.left == null) {
+                return currentNode;
+            } else {
+                currentNode = currentNode.left;
+            }
+
+        }
+
+        return null;
     }
 
     public static void main(String args[]) {
@@ -55,6 +129,10 @@ public class BinarySearchTree {
         bst.add(6);
         bst.add(9);
 
+        System.out.println(bst.findNode(3).data);
+
+        bst.delete(3);
+        System.out.println(bst.findNode(6).data);
 
     }
 }
