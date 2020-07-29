12074733c6336bee05eb1df89653de41078686a3
sourabh.saldi <Admin123#>
2020-07-29 11:37:50 +0530

    coin change ways

---
+++ src/algoexpert/medium/CoinChange.java
@@ -0,0 +1,29 @@
+package algoexpert.medium;
+
+public class CoinChange {
+
+    public int getChangeWays(int target, int[] nums) {
+
+        int[] ways = new int[target + 1];
+
+        ways[0] = 1;
+
+        for (int i = 1; i <= target; i++) {
+            ways[i] = 0;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 1; j <= target; j++) {
+                if (j >= nums[i]) {
+                    ways[j] += ways[j - nums[i]];
+                }
+            }
+        }
+
+        return ways[target];
+
+    }
+
+    public static void main(String args[]) {
+        System.out.println(new CoinChange().getChangeWays(10, new int[]{1, 5, 10, 25}));
+    }
+}
