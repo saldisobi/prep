232e60f1e089357a0718e94ba2badee5b3ef71e2
sourabh.saldi <Admin123#>
2020-08-31 13:27:52 +0530

    same tree

---
+++ src/amazon/IsSubTree.java
@@ -0,0 +1,44 @@
+package amazon;
+
+
+class IsSubTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null) {
+            return false;
+        } else {
+            return isSameTree(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t);
+        }
+    }
+
+    public boolean isSameTree(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        } else if (s == null || t == null) {
+            return false;
+        } else if (s.val == t.val) {
+            return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
+        } else {
+            return false;
+        }
+    }
+}
